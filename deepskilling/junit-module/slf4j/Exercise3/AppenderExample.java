8f22b08aff1ee63d65fd816316f1785dc5b45f1b
Ramharsh-aidev <ramharshdandekar@gmail.com>
2026-06-25 17:27:56 +0530

    chore(logging-mockito): add SLF4J Exercise1 and restructure Mockito exercises

---
+++ deepskilling/junit-module/slf4j/Exercise3/AppenderExample.java
@@ -0,0 +1,16 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppenderExample {
+    private static final Logger logger = LoggerFactory.getLogger(AppenderExample.class);
+
+    public static void main(String[] args) {
+        logger.trace("This is a TRACE message");
+        logger.debug("This is a DEBUG message");
+        logger.info("This is an INFO message");
+        logger.warn("This is a WARN message");
+        logger.error("This is an ERROR message");
+
+        System.out.println("Check the console output and the generated 'app.log' file!");
+    }
+}
